7851d2b52e2daa0f93d187c41dea3e4ab58477c7
220-TomiyamaAkifumi <akifumi_tomiyama@tbn.co.jp>
2017-12-14 15:50:46 +0900

    swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :users
+    end
+  end
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
